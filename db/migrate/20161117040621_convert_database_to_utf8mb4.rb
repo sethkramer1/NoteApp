00be39ea144fa0eea2bf5a364ef2bbb5019d42a9
Seth Kramer <sethaepiuva@gmail.com>
2016-11-18 11:35:21 -0500

    took away utf conversion

--- db/migrate/20161117040621_convert_database_to_utf8mb4.rb
+++
@@ -1,8 +0,0 @@
-class ConvertDatabaseToUtf8mb4 < ActiveRecord::Migration
-  def change
-    # for each table that will store unicode execute:
-   execute "ALTER TABLE table_name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-   # for each string/text column with unicode content execute:
-   execute "ALTER TABLE table_name CHANGE column_name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-  end
-end
